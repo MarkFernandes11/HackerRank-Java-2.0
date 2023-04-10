f35c5a3d8a19300513af6a23f5220ad745418a35
MarkFernandes11 <markfernandes66@gmail.com>
2023-04-10 17:56:59 +0530

    Added code for WelcomeToJava, JavaIfElse, JavaOutputFormatting, JavaStdinAndStdout1 and JavaStdinAndStdout2 problems

---
+++ src/com/practice/hackerrank/introduction/JavaStdinAndStdout1.java
@@ -0,0 +1,21 @@
+package com.practice.hackerrank.introduction;
+
+import java.util.Scanner;
+
+public class JavaStdinAndStdout1 {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int c = scanner.nextInt();
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		
+		scanner.close();
+	}
+
+}
